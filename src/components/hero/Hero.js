c7a4af454f687c890025381d3c803b358e645ce8
Sarthak Kekatpure <79007269+sarthak-op@users.noreply.github.com>
2024-07-29 13:25:38 +0530

    Update Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -41,9 +41,9 @@ const Hero = ({movies}) => {
                                             </div>
                                         </Link>
 
-                                        <div className="movie-review-button-container">
-                                            <Button variant ="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
-                                        </div>
+                                        // <div className="movie-review-button-container">
+                                        //     <Button variant ="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
+                                        // </div>
                                     </div>
                                 </div>
                             </div>
